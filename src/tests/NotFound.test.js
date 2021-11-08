56eb36a8e605e02d2ea8bbea67e932ff59610d4b
leonardo <leonardo.siepierski@outlook.com>
2021-11-08 15:39:48 -0300

    Requisito 4 OK

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o component Not Found', () => {
+  test('Testa o header h2', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/not-found');
+    const notFoundHeading = screen
+      .getByRole('heading', { level: 2, name: /page requested not found crying emoji/i });
+    expect(notFoundHeading).toBeInTheDocument();
+  });
+
+  test('Testa se aparece o gif', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/not-found');
+    const gif = screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i });
+    expect(gif).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
